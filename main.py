44f06167e0f30fc133dc48c0f7866ada2a01e3eb
William <willtheorangeguy@users.noreply.github.com>
2022-02-22 08:34:55 -0800

    Add 4 new commands

--- main.py
+++ main.py
@@ -2,9 +2,6 @@
 # (C) 2021 - 2022
 
 # Workout Options
-# Item complete - show time completed, start new time, show percentage, move to next workout
-# Skip
-# Workout finished - show total time, activities completed
 # Stats - time, percentage complete
 # Video - play video for that group
 
@@ -120,24 +117,31 @@ while run_activity == True:
         if select == "abs":
             print("You have completed: " + str((int(activity_num/len(abs_count)))) + "%")
             print("Please complete 2 Sets of " + str(abs_count[activity_num]) + " Reps of " + str(abs[activity_num]))
+            complete.append(abs[activity_num])
         elif select == "quads":
             print("You have completed: " + str((int(activity_num/len(quads_count)))) + "%")
             print("Please complete 2 Sets of " + str(quads_count[activity_num]) + " Reps of " + str(quads[activity_num]))
+            complete.append(quads[activity_num])
         elif select == "glutes":
             print("You have completed: " + str((int(activity_num/len(glutes_count)))) + "%")
             print("Please complete 2 Sets of " + str(glutes_count[activity_num]) + " Reps of " + str(glutes[activity_num]))
+            complete.append(glutes[activity_num])
         elif select == "triceps":
             print("You have completed: " + str((int(activity_num/len(triceps_count)))) + "%")
             print("Please complete 2 Sets of " + str(triceps_count[activity_num]) + " Reps of " + str(triceps[activity_num]))
+            complete.append(triceps[activity_num])
         elif select == "biceps":
             print("You have completed: " + str((int(activity_num/len(biceps_count)))) + "%")
             print("Please complete 2 Sets of " + str(biceps_count[activity_num]) + " Reps of " + str(biceps[activity_num]))
+            complete.append(biceps[activity_num])
         elif select == "back":
             print("You have completed: " + str((int(activity_num/len(back_count)))) + "%")
             print("Please complete 2 Sets of " + str(back_count[activity_num]) + " Reps of " + str(back[activity_num]))
+            complete.append(back[activity_num])
         elif select == "chest":
             print("You have completed: " + str((int(activity_num/len(chest_count)))) + "%")
             print("Please complete 2 Sets of " + str(chest_count[activity_num]) + " Reps of " + str(chest[activity_num]))
+            complete.append(chest[activity_num])
         print("")
         activity_num = activity_num + 1
         run_activity = True 
@@ -149,6 +153,7 @@ while run_activity == True:
             if activity_num < 6:
                 print("You have completed: " + str((int(activity_num/len(abs_count)*100))) + "%")
                 print("Please complete 2 Sets of " + str(abs_count[activity_num]) + " Reps of " + str(abs[activity_num]) + "\n")
+                complete.append(abs[activity_num])
             else:
                 print("You have completed all the workouts for this set!")
                 print("Run the `end` command to finish the workout. \n")
@@ -156,6 +161,7 @@ while run_activity == True:
             if activity_num < 6:
                 print("You have completed: " + str((int(activity_num/len(quads_count)*100))) + "%")
                 print("Please complete 2 Sets of " + str(quads_count[activity_num]) + " Reps of " + str(quads[activity_num]) + "\n")
+                complete.append(quads[activity_num])
             else:
                 print("You have completed all the workouts for this set!")
                 print("Run the `end` command to finish the workout. \n")
@@ -163,6 +169,7 @@ while run_activity == True:
             if activity_num < 6:
                 print("You have completed: " + str((int(activity_num/len(glutes_count)*100))) + "%")
                 print("Please complete 2 Sets of " + str(glutes_count[activity_num]) + " Reps of " + str(glutes[activity_num]) + "\n")
+                complete.append(glutes[activity_num])
             else:
                 print("You have completed all the workouts for this set!")
                 print("Run the `end` command to finish the workout. \n")
@@ -170,6 +177,7 @@ while run_activity == True:
             if activity_num < 6:
                 print("You have completed: " + str((int(activity_num/len(triceps_count)*100))) + "%")
                 print("Please complete 2 Sets of " + str(triceps_count[activity_num]) + " Reps of " + str(triceps[activity_num]) + "\n")
+                complete.append(triceps[activity_num])
             else:
                 print("You have completed all the workouts for this set!")
                 print("Run the `end` command to finish the workout. \n")
@@ -177,6 +185,7 @@ while run_activity == True:
             if activity_num < 5:
                 print("You have completed: " + str((int(activity_num/len(biceps_count)*100))) + "%")
                 print("Please complete 2 Sets of " + str(biceps_count[activity_num]) + " Reps of " + str(biceps[activity_num]) + "\n")
+                complete.append(biceps[activity_num])
             else:
                 print("You have completed all the workouts for this set!")
                 print("Run the `end` command to finish the workout. \n")
@@ -184,6 +193,7 @@ while run_activity == True:
             if activity_num < 5:
                 print("You have completed: " + str((int(activity_num/len(back_count)*100))) + "%")
                 print("Please complete 2 Sets of " + str(back_count[activity_num]) + " Reps of " + str(back[activity_num]) + "\n")
+                complete.append(back[activity_num])
             else:
                 print("You have completed all the workouts for this set!")
                 print("Run the `end` command to finish the workout. \n")
@@ -191,16 +201,55 @@ while run_activity == True:
             if activity_num < 6:
                 print("You have completed: " + str((int(activity_num/len(chest_count)*100))) + "%")
                 print("Please complete 2 Sets of " + str(chest_count[activity_num]) + " Reps of " + str(chest[activity_num]) + "\n")
+                complete.append(chest[activity_num])
             else:
                 print("You have completed all the workouts for this set!")
                 print("Run the `end` command to finish the workout. \n")
         activity_num = activity_num + 1
         run_activity = True 
+    elif activity.lower() == "skip":
+        now = datetime.now()
+        print("You are in the " + select + " muscle group. ")
+        print("The current time is: " + str(time.strftime("%H:%M:%S")) + ". " + str(now-start) + " has elapsed.")
+        complete.pop(-1)
+        print("Activity skipped! Run the `next` command to move on. \n")
+        run_activity = True
+    elif activity.lower() == "end":
+        now = datetime.now()
+        print("You have completed the " + select + " muscle group.")
+        print("It took " + str(now-start) + " to complete this workout.")
+        print("The following activities were completed: ")
+        for i in range(len(complete)):
+            print(str(i+1) + ". " + str(complete[i]))
+        print("Congratulations! \n")
+        run_activity = False
     elif activity.lower() == "quit":
         run_activity = False
         exit()
         break 
+    elif activity.lower() == "help":
+        print("PyWorkout - (C) 2021-2022")
+        print("Any of these options are available: ")
+        print("list    Lists the workout activities by muscle group.")
+        print("start   Starts the workout and displays the first workout activity.")
+        print("next    Moves to the next workout activity.")
+        print("skip    Skips the current workout activity.")
+        print("end     Completes the workout and display full workout statistics.")
+        print("stats   Shows workout statistics at any point and time.")
+        print("video   Opens the workout video assigned to each muscle group.")
+        print("help    Prints this help text.")
+        print("quit    Ends the program.")
+        print("More documentation can be found on Github. Enjoy using the program! \n")
+        run_activity = True
     else:
         print("Sorry that is not an option. Please see this list of options below:")
-        print("")
-        run_activity = True 
+        print("list    Lists the workout activities by muscle group.")
+        print("start   Starts the workout and displays the first workout activity.")
+        print("next    Moves to the next workout activity.")
+        print("skip    Skips the current workout activity.")
+        print("end     Completes the workout and display full workout statistics.")
+        print("stats   Shows workout statistics at any point and time.")
+        print("video   Opens the workout video assigned to each muscle group.")
+        print("help    Prints a similar help text.")
+        print("quit    Ends the program. \n")
+        run_activity = True
